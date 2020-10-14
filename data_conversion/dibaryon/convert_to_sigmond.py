8bcb7a9ec91eab58ec83c9ee0e468f5ff6b5693f
Andrew D. Hanlon <Andrew.Hanlon@pitt.edu>
2020-10-14 00:38:03 -0400

    updated dibaryon data scripts

--- data_conversion/dibaryon/convert_to_sigmond.py
+++ data_conversion/dibaryon/convert_to_sigmond.py
@@ -14,7 +14,7 @@ FORCE_HERM = True
 base_data_dir = "/media/ext2/research/data/raw_dibaryon/"
 
 #ensembles_to_do = ['U102', 'U103', 'B450', 'H101', 'H200', 'A653', 'N300', 'N202', 'E1', 'E5']
-ensembles_to_do = ['U103']
+ensembles_to_do = ['B450', 'N300']
 
 def main():
   for ensemble in defs.ensembles:
