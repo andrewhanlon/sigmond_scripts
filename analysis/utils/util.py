a3deab1d1749c6a3bad43e356f6747e572c2f4c9
Sarah Skinner <skinnersarah93@gmail.com>
2021-08-29 12:24:19 -0400

    More error output for generating pdfs

--- analysis/utils/util.py
+++ analysis/utils/util.py
@@ -432,14 +432,17 @@ def add_image(figure, rel_dir, pdf_file, width="1.0", caption="", view=True):
 
 
 def compile_pdf(doc, filename, compiler=None):
-  logging.info(f"Compiling LaTeX: {filename}.tex")
   doc.generate_tex(filename)
+  logging.info(f"Created LaTeX file: {filename}.tex")
   try:
     doc.generate_pdf(filename, clean=True, clean_tex=False, compiler=compiler, compiler_args=['-f'])
     logging.info(f"Created PDF: {filename}.pdf")
   except subprocess.CalledProcessError as err:
     print(err)
-    logging.info(f"Unable to create PDF: {filename}.pdf")
+    logging.warning(f"Unable to create PDF: {filename}.pdf")
+  except pylatex.errors.CompilerError:
+    logging.warning(f"No LaTeX compiler available")
+    logging.warning(f"Unable to create PDF: {filename}.pdf")
     
 def write_tikz(tikz, filename):
   with open(filename, 'w', encoding='utf-8') as f_hand:
@@ -479,7 +482,7 @@ def dirGrace2pdf(dirname, margins='0'):
 
   except FileNotFoundError:
     print("No such directory: {}".format(dirname))
-
+  
   os.chdir(cwd)
   logging.info("Finished converting grace plots")
 
